710b988ece0f6158ccf156744b20dc64f21681f4
Andrew Thompson <amtunknown@Andrews-MBP.lan>
2022-07-13 22:28:48 -0700

    SQL database attached utilizing PostGRES. Form submission enable for root/facts/new with redirect to root/facts

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 # factory
 def create_app():
+    #config
     app = Flask(__name__)
+
+    #database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:zt7FsghsnpCjbB@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     # index
     @app.route('/')
